c956211aa6fdc608c924cf62a5ddc08fb23599d6
Daniel Garnier <dgarnier@timsoft.com>
2016-04-06 16:36:54 +0200

    + Add a proxy server

---
+++ tools/server.js
@@ -0,0 +1,21 @@
+var express = require('express');
+var request = require('request');
+var fs      = require('fs');
+var server  = express();
+
+var api = JSON.parse(fs.readFileSync('api.json', 'utf8'));
+
+
+server.use(express.static('../site/'));
+server.get('/', function(req, res, next) {
+    // Just send the index.html for other files to support HTML5Mode
+    res.sendFile('../site/index.html', { root: __dirname });
+});
+server.all('/api', function(req, res) {
+    var url = api.baseUri + req.url.replace("/api", "") + "&keyapp=" + api.key;
+    req.pipe(request(url)).pipe(res);
+    //res.send('Hello World!');
+});
+server.listen(3000, function () {
+  console.log('Example app listening on port 3000!');
+});
